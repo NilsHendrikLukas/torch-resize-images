2ed464d646681aba4bcf5d96e00eaed1e202d387
anonymous <nlukas@uwaterloo.ca>
2021-03-19 10:52:30 -0400

    Make script runnable from cmd line.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='torch-resize-images',
-    version='0.1.2',
-    description='A python library to resize images using PyTorch',
+    version='0.1.3',
+    description='A Python library to resize images using PyTorch',
     url='https://github.com/NilsHendrikLukas/torch-resize-images',
     author='Nils Lukas',
     author_email='nlukas@uwaterloo.ca',
-    scripts=["./torch_resize_new"],
+    scripts=["./torch_resize"],
     license='MIT',
     keywords='resize image pytorch',
-    packages=find_packages('src'),
+    packages=find_packages(),
     install_requires=['Pillow>=5.1.0', 'torch', 'torchvision', 'gputil', 'tqdm']
 )
